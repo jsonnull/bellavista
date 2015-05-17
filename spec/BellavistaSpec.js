68da1d4c1bc7e1aea64077acd5022e3da517a94e
Jason Nall <jsonnull@gmail.com>
2015-05-17 00:00:04 -0400

    Adding test files

---
+++ spec/BellavistaSpec.js
@@ -0,0 +1,13 @@
+describe("Bellavista", function() {
+    var bellavista = require("../dist/bellavista.js");
+    var app;
+
+    beforeEach(function() {
+        app = bellavista();
+    });
+
+    it("should have stores for components and routes", function() {
+        expect(app.components).toBeDefined();
+        expect(app.routes).toBeDefined();
+    });
+});
